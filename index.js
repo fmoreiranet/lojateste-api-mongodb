42f29cfd28bcc71967fd9192e09d161d7fcee73b
Fabiano Moreira <fabianomoreira.net@gmail.com>
2023-01-10 17:59:26 -0300

    teste

--- index.js
+++ index.js
@@ -15,7 +15,22 @@ app.use(express.json());
 app.get("/", (req, res) => {
     console.log(req);
     res.status(200).json({ message: "Bem vindo!" });
-})
+});
 
-//Listen
-app.listen(3000);
+
+//Connect Data Base: MondoDB
+const DB_USER = 'lojatestAdmin';
+const DB_PASS = encodeURIComponent('ya09S5EMVSLHxQ4s');
+const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster-lojateste.wdjpjoi.mongodb.net/?retryWrites=true&w=majority`;
+const uriLocal = "mongodb://127.0.0.1:27017/loja-teste?directConnection=true";
+
+mongoose.set('strictQuery', false);
+mongoose.connect(uriLocal)
+    .then(res => {
+        console.log("Conectado!");
+        //Listen
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.error("Error Connect: ", err);
+    });
