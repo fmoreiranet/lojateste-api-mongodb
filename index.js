ea1db6e1e50d1adf1faa777bc71ea894503fd761
Fabiano Moreira <fabianomoreira.net@gmail.com>
2023-01-18 13:41:19 -0300

    publish

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const dotenv = require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const dotenv = require('dotenv').config();
 
 const auth = require('./services/auth');
 
